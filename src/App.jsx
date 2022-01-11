cb651e02f79112ee46ba94657c8258953436727d
sorci89 <sore.eszter@gmail.com>
2022-01-11 15:07:55 +0100

    useState 3 buttons

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,9 @@ const App = () => {
     setter(!isLandingPage)
   }
 
+  let [pageName, setPageName] = useState("Home");
+  
+
 
   return (
     <div>
@@ -46,6 +49,15 @@ const App = () => {
       </div>
       <button onClick={toggleDisplay}>Click me!</button>
       {/*<DivFunction />*/}
+      <div>
+        <button onClick={()=> setPageName ("Home")}>Home</button>
+        <button onClick={()=> setPageName ("About")}>About</button>
+        <button onClick={()=> setPageName ("Landing")}>Landing</button>
+      
+        {pageName === "Home" && <div>Content of Home Page</div>}
+        {pageName === "About" && <div><h1>Content of About</h1></div>}
+        {pageName === "Landing" && <div><input type="text"></input></div>}
+      </div>
     </div>
   );
 };
