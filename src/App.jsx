02327d621f20372445dffab16ab7f7decb780a9d
sorci89 <sore.eszter@gmail.com>
2022-01-12 15:22:34 +0100

    durin class 01.12.

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,27 @@ const App = () => {
   let [pageName, setPageName] = useState("Home");
   
 
+  const inputValue = "Ez az érték"
 
+  let [inputValue2, setInputValue2] = useState("")
+  console.log(inputValue2)
+  //console.log(typeof inputValue2)
+
+  const cars = [
+    {brand: "Toyota", model: "yaris", topSpeed: "190km/h"},
+    {brand: "Mercedes", model: "C63", topSpeed: "260km/h"},
+    {brand: "Mazda", model: "6", topSpeed: "210km/h"},
+    {brand: "Mazda", model: "Civic", topSpeed: "200km/h"},
+    {brand: "Audi", model: "A4", topSpeed: "220km/h"},
+  ]
+ 
+  // const transformToDiv = (carObject) => <div>{carObject.brand} - {carObject.model} ({carObject.topSpeed})</div>
+  
+
+  
   return (
-    <div>
-      Hello
+    <div className="App">
+        Hello
       <div>{
         isHomePage ? 
         <div>Home Page</div> : 
@@ -58,6 +75,18 @@ const App = () => {
         {pageName === "About" && <div><h1>Content of About</h1></div>}
         {pageName === "Landing" && <div><input type="text"></input></div>}
       </div>
+      <hr />
+
+      <input type="text" value={inputValue}></input>
+      <input type="text" name="submit" onChange={(event)=> setInputValue2(event.target.value)}></input>
+      {/* {inputValue2.length >= 3 ? <button>Submit</button> : <button disabled>Submit</button>} */}
+      <button disabled={inputValue2.length < 3}>Submit</button>
+      {inputValue2.length < 3 && <p>Min 3 char!</p>}
+      <hr />
+      {/* {cars.map(car => <div>{car.brand} - {car.model} ({car.topSpeed})</div>)} */}
+      {cars.filter(car => car.brand.slice(0, inputValue2.length) === inputValue2).map(car => <div>{car.brand} - {car.model} ({car.topSpeed})</div>)}
+      {/* {cars.filter(car => car.brand.startsWith(inputValue2)).map((car, index) => <div key={index}>{car.brand} - {car.model} ({car.topSpeed})</div>)} */}
+      {/* <ul>{cars.map((element, index)=> (<li key={index}>{element.brand} {element.model} {element.topSpeed}</li>))}</ul> */}
     </div>
   );
 };
