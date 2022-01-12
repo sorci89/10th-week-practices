9b8c24f74dddbdf836992c3ab4cb722409c9fb72
sorci89 <sore.eszter@gmail.com>
2022-01-12 13:04:56 +0100

    navbar mod

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <div className="navBar">
-      <h1 className="navLogo">Logo</h1>
+      <h1 className="navLogo">Team 4 Pizzeria</h1>
       <h2 className="navTile">Rólunk</h2>
       <h2 className="navTile">Menü</h2>
     </div>
