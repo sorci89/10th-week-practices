40c3a97d1c9df3b110ba169ca48ea0c8f7df1f54
sorci89 <sore.eszter@gmail.com>
2022-01-12 22:28:00 +0100

    intro 100%

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,7 +10,27 @@ function Intro() {
       <div className="introPicture">
         <img src={Picture2} alt="picture2" id="introPicture2"></img>
       </div>
-      <div className="introText"></div>
+      <div className="introText">
+        <p>
+          Az első Team 4 Pizzéria 1958-ban az Amerikai Egyesült Államokban nyílt
+          meg.
+        </p>
+        <p>
+          Jelenleg több mint 90 országban van Pizza Hut étterem, jelentős részük
+          nem a cég tulajdonában van, hanem franchise szerződés alapján működik.
+        </p>
+        <p>
+          Magyarországon 1992. április 1-jén Budapesten, a Király utca és a
+          Nagykörút sarkán nyitotta meg első éttermét, melyet további 15 egység
+          követett, Budapesten kívül Győrben, Pécsett, Miskolcon és Szegeden is.
+        </p>
+        <p>
+          Az amerikai típusú serpenyős (deep pan) pizza kedveltté vált a
+          korábban ismert olaszos vékony pizzatészták mellett. A Pizza Hut
+          vezette be a szervezett formában működő házhoz szállítási
+          szolgáltatást Magyarországon.
+        </p>
+      </div>
     </div>
   );
 }
